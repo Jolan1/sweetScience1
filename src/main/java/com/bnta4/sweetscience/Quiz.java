6f580c227d51fd8ac329c55d306e225c8e1d6d4c
fatsmao <maofatma@gmail.com>
2021-09-11 12:07:28 +0100

    form added

--- src/main/java/com/bnta4/sweetscience/Quiz.java
+++ src/main/java/com/bnta4/sweetscience/Quiz.java
@@ -1,2 +1,35 @@
-package com.bnta4.sweetscience;public class Quiz {
+package com.bnta4.sweetscience;
+
+import java.util.List;
+
+public class Quiz {
+    public String title;
+    public List<Question> questions;
+
+    public Quiz(String title, List<Question> questions) {
+        this.title = title;
+        this.questions = questions;
+    }
+
+    public static class Question {
+        public String question;
+        public String id;
+        public List<Answer> answers;
+
+        public Question(String question, String id, List<Answer> answers) {
+            this.question = question;
+            this.id = id;
+            this.answers = answers;
+        }
+    }
+
+    public static class Answer {
+        public String answer;
+        public String id;
+
+        public Answer(String answer, String id) {
+            this.answer = answer;
+            this.id = id;
+        }
+    }
 }
--- src/main/java/com/bnta4/sweetscience/sweetScienceController.java
+++ src/main/java/com/bnta4/sweetscience/sweetScienceController.java
@@ -1,2 +1,31 @@
-package com.bnta4.sweetscience;public class sweetScienceController {
+package com.bnta4.sweetscience;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.*;
+import java.sql.SQLOutput;
+
+
+@RestController
+@RequestMapping(path= "api/sweetscience")
+public class sweetScienceController {
+
+    @GetMapping
+    public String sayHello(){
+        System.out.println("Hello!");
+        return "Hello";
+
+    }
+
+    @GetMapping ("user")
+
+
+    @GetMapping ("element")
+
 }
+
+
