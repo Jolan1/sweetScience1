ed6cd55e4cd680680e0d5a88b4c2303a2c9071f0
Mary1-5 <marymansha15@gmail.com>
2021-09-14 10:36:48 +0100

    updated

--- src/main/java/com/bnta4/sweetscience/sweetScienceController.java
+++ src/main/java/com/bnta4/sweetscience/sweetScienceController.java
@@ -1,5 +1,6 @@
 package com.bnta4.sweetscience;
 
+import com.bnta4.sweetscience.user.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +14,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "api/sweetScience")
 public class sweetScienceController {
 
-    @GetMapping
-    public String sayHello() {
-        System.out.println("Hello!");
-        return "Hello";
-
-    }
-
     @GetMapping("question/{number}")
     public Quiz.Question getQuestion(@PathVariable int number) {
         return quizQuestions.get(number - 1);
@@ -30,55 +24,65 @@ public class sweetScienceController {
                     new Quiz.Question("Would you rather meditate or pray?", "Q1",
                             List.of(
                                     new Quiz.Answer("Both.", "a"),
-                                    new Quiz.Answer("Meditate.", "a"),
-                                    new Quiz.Answer("Pray.", "b"),
-                                    new Quiz.Answer("I'm more of a deep thinker.", "c")
+                                    new Quiz.Answer("Meditate.", "b"),
+                                    new Quiz.Answer("Pray.", "c"),
+                                    new Quiz.Answer("I'm more of a deep thinker.", "d")
                             )),
 
                     new Quiz.Question("How did you spend your last birthday?", "Q2",
                             List.of(
                                     new Quiz.Answer(" I spent it with family.", "a"),
-                                    new Quiz.Answer("I spent the day alone.", "a"),
-                                    new Quiz.Answer("I spent it with friends.", "b"),
-                                    new Quiz.Answer("I spent it at work.", "c")
+                                    new Quiz.Answer("I spent the day alone.", "b"),
+                                    new Quiz.Answer("I spent it with friends.", "c"),
+                                    new Quiz.Answer("I spent it at work.", "d")
                             )),
                     new Quiz.Question("Which best describes you?", "Q3",
                             List.of(
                                     new Quiz.Answer("Authentic.", "a"),
-                                    new Quiz.Answer("Relaxed.", "a"),
-                                    new Quiz.Answer("Driven.", "b"),
-                                    new Quiz.Answer("Reliable.", "c")
+                                    new Quiz.Answer("Relaxed.", "b"),
+                                    new Quiz.Answer("Driven.", "c"),
+                                    new Quiz.Answer("Reliable.", "d")
                             )),
 
                     new Quiz.Question("How would you build a fire?", "Q4",
                             List.of(
                                     new Quiz.Answer("I would rub two sticks together.", "a"),
-                                    new Quiz.Answer("I would use matches.", "a"),
-                                    new Quiz.Answer("Gasoline solves everything.", "b"),
-                                    new Quiz.Answer("I would use a fire starter.", "c")
+                                    new Quiz.Answer("I would use matches.", "b"),
+                                    new Quiz.Answer("Gasoline solves everything.", "c"),
+                                    new Quiz.Answer("I would use a fire starter.", "d")
                             )),
                     new Quiz.Question("What movie genre do you watch most often?", "Q5",
                             List.of(
                                     new Quiz.Answer("Horror", "a"),
-                                    new Quiz.Answer("Rom-Com", "a"),
-                                    new Quiz.Answer("Action/Adventure", "b"),
-                                    new Quiz.Answer("Documentary", "c")
+                                    new Quiz.Answer("Rom-Com", "b"),
+                                    new Quiz.Answer("Action/Adventure", "c"),
+                                    new Quiz.Answer("Documentary", "d")
 
                             )),
                     new Quiz.Question("What happens to your soul when you die?", "Q6",
                             List.of(
                                     new Quiz.Answer("I believe it goes to heaven.", "a"),
-                                    new Quiz.Answer("I believe in reincarnation.", "a"),
-                                    new Quiz.Answer("I have no idea.", "b"),
-                                    new Quiz.Answer("I hope I get to go to space.", "c")
+                                    new Quiz.Answer("I believe in reincarnation.", "b"),
+                                    new Quiz.Answer("I have no idea.", "c"),
+                                    new Quiz.Answer("I hope I get to go to space.", "d")
                             )),
                     new Quiz.Question("Which describes your soul best?", "Q7",
                             List.of(
                                     new Quiz.Answer("Unique", "a"),
-                                    new Quiz.Answer("Free", "a"),
-                                    new Quiz.Answer("Kind", "b"),
-                                    new Quiz.Answer("Old", "c")
+                                    new Quiz.Answer("Free", "b"),
+                                    new Quiz.Answer("Kind", "c"),
+                                    new Quiz.Answer("Old", "d")
                             )));
+
+    @PostMapping("answer/{value}")
+    public String getAnswer(@PathVariable String value) {
+        return value;
+    }
+    //get value inputted into the questions
+//add these to a list
+//do the for loop through the list which does the calculation.
+//another link for 'finish' quiz -- should take you to create a login/ details page which saves details to a database.
+
 }
 
 
